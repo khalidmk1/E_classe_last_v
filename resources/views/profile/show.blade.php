2039aa95572ebe7c948882303b51aaff8a187d2b
khalidmk1 <103368578+khalidmk1@users.noreply.github.com>
2023-07-27 10:24:53 +0100

    test

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -110,6 +110,9 @@
                         {{ $profile->subject }}
                     </h6>
                     <p class="proile-rating">Suivez de c'est cours:
+                        @php
+                            use App\Models\Flow;
+                        @endphp
                         <span>{{ App\Models\folow::where('user_id', auth()->user()->id)->get()->count() }}</span>
                     </p>
                     <ul class="nav nav-tabs" id="myTab" role="tablist">
