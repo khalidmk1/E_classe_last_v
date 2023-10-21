da1b4b9e4b3ad790f0371b55925cc3b5774ded3a
khalidmk1 <103368578+khalidmk1@users.noreply.github.com>
2023-10-21 12:30:24 +0100

    update tarif.blade.php

--- resources/views/landing_page/tarif.blade.php
+++ resources/views/landing_page/tarif.blade.php
@@ -1,148 +1,132 @@
 @extends('master.master_landing_page')
 
-@section("content")
-<style>
-    h5{
-        color: black !important;
-    }
-</style>
+@section('content')
+    <style>
+        h5 {
+            color: black !important;
+        }
+    </style>
 
-  <!--Main layout-->
-  <main class="my-4">
-    <div class="container">
-      <!--Section: Content-->
-      <section class="text-center ">
-        
-        <div class="row gx-lg-5">
+    <!--Main layout-->
+    <main class="my-4">
+        <div class="container">
+            <!--Section: Content-->
+            <section class="text-center ">
 
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4">
+                <div class="row gx-lg-5 justify-content-center">
 
-            <!-- Card -->
-            <div class="card">
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4">
 
-              <div class="card-header bg-white py-3">
-                <p class="text-uppercase small mb-2"><strong>Plan d'Abonnement Mensuel pour les Formateurs
-                </strong></p>
-                <h5 class="mb-0">200 DH</h5>
-              </div>
+                        <!-- Card -->
+                        <div class="card">
 
-              <div class="card-body">
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                </ul>
-              </div>
+                            <div class="card-header bg-white py-3">
+                                <p class="text-uppercase small mb-2"><strong>Pack d'Abonnement Mensuel pour les Formateurs
+                                    </strong></p>
+                                <h5 class="mb-0">200 DH</h5>
+                            </div>
 
-              <div class="card-footer bg-white py-3">
-                <button type="button" class="btn btn-success btn-sm">Get it</button>
-              </div>
+                            <div class="card-body">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">accéder à votre espace de travail</li>
+                                    <li class="list-group-item">Notre site Web fera activement la promotion des profils des
+                                        formateurs inscrits, en mettant en valeur leurs compétences et leurs offres de
+                                        cours.</li>
+                                    <li class="list-group-item">Les formateurs peuvent avoir un maximum de 4 étudiants par
+                                        session, avec un coût additionnel de 20 DH par étudiant.
+                                    </li>
+                                    <li class="list-group-item">3 séances par semaine.</li>
+                                </ul>
+                            </div>
 
-            </div>
-            <!-- Card -->
+                            <div class="card-footer bg-white py-3">
+                                <button type="button" class="btn btn-success btn-sm">Obtenir</button>
+                            </div>
 
-          </div>
-          <!--Grid column-->
+                        </div>
+                        <!-- Card -->
 
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4">
+                    </div>
+                    <!--Grid column-->
 
-            <!-- Card -->
-            <div class="card border border-primary">
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4">
 
-              <div class="card-header bg-white py-3">
-                <p class="text-uppercase small mb-2"><strong>Essential</strong></p>
-                <h5 class="mb-0">$19/month</h5>
-              </div>
+                        <!-- Card -->
+                        <div class="card border border-primary">
 
-              <div class="card-body">
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                </ul>
-              </div>
+                            <div class="card-header bg-white py-3">
+                                <p class="text-uppercase small mb-2"><strong>Pack Silver pour les Formateurs</strong></p>
+                                <h5 class="mb-0">350 DH/8 Mois</h5>
+                            </div>
 
-              <div class="card-footer bg-white py-3">
-                <button type="button" class="btn btn-primary btn-sm">Buy now</button>
-              </div>
+                            <div class="card-body">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">accéder à votre espace de travail.
+                                    </li>
+                                    <li class="list-group-item">Notre site Web fera activement la promotion des profils des
+                                        formateurs inscrits, en mettant en valeur leurs compétences et leurs offres de
+                                        cours.</li>
+                                    <li class="list-group-item">Les formateurs peuvent avoir un maximum de 7 étudiants par
+                                        session, avec un coût additionnel de 20 DH par étudiant.</li>
+                                    <li class="list-group-item">5 séances par semaine.</li>
+                                    <li class="list-group-item">Les formateurs premium bénéficient d'un support dédié et
+                                        d'une assistance technique pour garantir une expérience optimale.
+                                    </li>
+                                </ul>
+                            </div>
 
-            </div>
-            <!-- Card -->
+                            <div class="card-footer bg-white py-3">
+                                <button type="button" class="btn btn-primary btn-sm">Acheter maintenant</button>
+                            </div>
 
-          </div>
-          <!--Grid column-->
+                        </div>
+                        <!-- Card -->
 
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4">
+                    </div>
+                    <!--Grid column-->
 
-            <!-- Card -->
-            <div class="card">
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4">
 
-              <div class="card-header bg-white py-3">
-                <p class="text-uppercase small mb-2"><strong>Advanced</strong></p>
-                <h5 class="mb-0">$49/month</h5>
-              </div>
+                        <!-- Card -->
+                        <div class="card">
 
-              <div class="card-body">
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                </ul>
-              </div>
+                            <div class="card-header bg-white py-3">
+                                <p class="text-uppercase small mb-2"><strong>Pack Gold pour les Formateurs</strong></p>
+                                <h5 class="mb-0">500 DH/12 Mois</h5>
+                            </div>
 
-              <div class="card-footer bg-white py-3">
-                <button type="button" class="btn btn-info btn-sm">Buy now</button>
-              </div>
+                            <div class="card-body">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">accéder à votre espace de travail.</li>
+                                    <li class="list-group-item">Notre site Web fera activement la promotion des profils des
+                                      formateurs inscrits, en mettant en valeur leurs compétences et leurs offres de
+                                      cours.</li>
+                                    <li class="list-group-item">Les formateurs peuvent avoir un maximum de 20 étudiants par
+                                      session</li>
+                                    <li class="list-group-item">llimité séances par semaine.</li>
+                                    <li class="list-group-item">Les formateurs premium bénéficient d'un support dédié et
+                                      d'une assistance technique pour garantir une expérience optimale.</li>
+                                </ul>
+                            </div>
 
-            </div>
-            <!-- Card -->
+                            <div class="card-footer bg-white py-3">
+                                <button type="button" class="btn btn-info btn-sm">Acheter maintenant</button>
+                            </div>
 
-          </div>
-          <!--Grid column-->
+                        </div>
+                        <!-- Card -->
 
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4">
+                    </div>
+                    <!--Grid column-->
 
-            <!-- Card -->
-            <div class="card">
 
-              <div class="card-header bg-white py-3">
-                <p class="text-uppercase small mb-2"><strong>Enterprise</strong></p>
-                <h5 class="mb-0">$189/month</h5>
-              </div>
-
-              <div class="card-body">
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                </ul>
-              </div>
-
-              <div class="card-footer bg-white py-3">
-                <button type="button" class="btn btn-info btn-sm">Buy now</button>
-              </div>
-
-            </div>
-            <!-- Card -->
-
-          </div>
-          <!--Grid column-->
-
+                </div>
+            </section>
+            <!--Section: Content-->
         </div>
-      </section>
-      <!--Section: Content-->
-    </div>
-  </main>
-  <!--Main layout-->
-    
+    </main>
+    <!--Main layout-->
 @endsection
