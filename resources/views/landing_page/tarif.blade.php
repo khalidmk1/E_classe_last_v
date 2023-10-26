fe0ac802c9dc78551c4a53c586c4914c0a6ca5f5
khalidmk1 <103368578+khalidmk1@users.noreply.github.com>
2023-10-26 14:56:22 +0100

    update tarif page

--- resources/views/landing_page/tarif.blade.php
+++ resources/views/landing_page/tarif.blade.php
@@ -24,7 +24,7 @@
                             <div class="card-header bg-white py-3">
                                 <p class="text-uppercase small mb-2"><strong>Pack d'Abonnement Mensuel pour les Formateurs
                                     </strong></p>
-                                <h5 class="mb-0">500 DH</h5>
+                                <h5 class="mb-0">200 DH</h5>
                             </div>
 
                             <div class="card-body">
@@ -58,7 +58,7 @@
 
                             <div class="card-header bg-white py-3">
                                 <p class="text-uppercase small mb-2"><strong>Pack Silver pour les Formateurs</strong></p>
-                                <h5 class="mb-0">450 DH/6 Mois</h5>
+                                <h5 class="mb-0">450 DH/3 Mois</h5>
                             </div>
 
                             <div class="card-body">
@@ -95,7 +95,41 @@
 
                             <div class="card-header bg-white py-3">
                                 <p class="text-uppercase small mb-2"><strong>Pack Gold pour les Formateurs</strong></p>
-                                <h5 class="mb-0">400 DH/12 Mois</h5>
+                                <h5 class="mb-0">750  DH/6 Mois</h5>
+                            </div>
+
+                            <div class="card-body">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">accéder à votre espace de travail.</li>
+                                    <li class="list-group-item">Notre site Web fera activement la promotion des profils des
+                                      formateurs inscrits, en mettant en valeur leurs compétences et leurs offres de
+                                      cours.</li>
+                                    <li class="list-group-item">Les formateurs peuvent avoir un maximum de 20 étudiants par
+                                      session</li>
+                                    <li class="list-group-item">llimité séances par semaine.</li>
+                                    <li class="list-group-item">Les formateurs premium bénéficient d'un support dédié et
+                                      d'une assistance technique pour garantir une expérience optimale.</li>
+                                </ul>
+                            </div>
+
+                            <div class="card-footer bg-white py-3">
+                                <button type="button" class="btn btn-info btn-sm">Acheter maintenant</button>
+                            </div>
+
+                        </div>
+                        <!-- Card -->
+
+                    </div>
+                    <!--Grid column-->
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4">
+
+                        <!-- Card -->
+                        <div class="card">
+
+                            <div class="card-header bg-white py-3">
+                                <p class="text-uppercase small mb-2"><strong>Pack Gold pour les Formateurs</strong></p>
+                                <h5 class="mb-0">1250 DH/12 Mois</h5>
                             </div>
 
                             <div class="card-body">
